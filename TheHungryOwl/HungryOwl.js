5f9e5f3541df9bca3c545a90206261c255d616f2
Holly Maze <hollymaze@gmail.com>
2019-10-28 01:28:37 -0400

    large update to Hungry Owl pages

--- TheHungryOwl/HungryOwl.js
+++ TheHungryOwl/HungryOwl.js
@@ -1,30 +1,24 @@
-// var $window = $(window)
-
-// $window.on('load', function() {
-// 				$('.nav-item').poptrox({
-// 					// caption: function($a) { return $a.next('h3').text(); },
-// 					// overlayColor: '#2c2c2c',
-// 					// overlayOpacity: 0.85,
-// 					// popupCloserText: '',
-// 					// popupLoaderText: '',
-// 					// selector: '.nav-item href',
-// 					usePopupCaption: true,
-// 					usePopupDefaultStyling: false,
-// 					usePopupEasyClose: false,
-// 					usePopupNav: true,
-// 					windowMargin: (breakpoints.active('<=small') ? 0 : 50)
-// 				});
-// });
-
-
 //variables to hold values of input from online order form
 
 //customer name
-let custName = $("#firstName").val();
-let custLast = $("#lastName").val();
+// let custName = $("#firstName").val();
+// let custLast = $("#lastName").val();
 
 //delivery or pickup
-let orderMethod = $("method").val();
+// let orderMethod = $("method").val();
 
 //to hold value of customer lunch selection (shows day not meal type ex: Thursday)
-let lunchChoice = $("#whichLunch").val();
+// let lunchChoice = $("#whichLunch").val();
+
+let placeOrder = document.getElementById("submit");
+
+//eventually the alert message will be a pop-up window and will display a 
+//message similar to "Thank you (customer name), your order of (food item)
+//will be ready for (pickup or delivery) on (day) between 11:00am and 3:00pm
+//(or whatever operation times are currently)"
+
+// placeOrder.addEventListener("click", function() {
+	//if all required fields in order form are filled out
+	// alert("Thank you, your order has been recieved!");
+	//else do not display alert
+// })
